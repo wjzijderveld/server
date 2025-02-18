44e0f43254d24aadddf7ec293986365d8c12f761
Marcel van der Veldt <m.vanderveldt@outlook.com>
2025-02-18 01:21:59 +0100

    Fix typo in spotify default name

--- music_assistant/providers/spotify/__init__.py
+++ music_assistant/providers/spotify/__init__.py
@@ -293,7 +293,7 @@ class SpotifyProvider(MusicProvider):
         if self._sp_user:
             postfix = self._sp_user["display_name"]
             return f"{self.manifest.name}: {postfix}"
-        return super().name
+        return super().default_name
 
     async def search(
         self, search_query: str, media_types=list[MediaType], limit: int = 5
