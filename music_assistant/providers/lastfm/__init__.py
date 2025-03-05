ef2462f4edf374850def913140ad49cf7d8f8fe5
Willem-Jan <wjzijderveld@protonmail.com>
2025-03-05 10:46:35 +0100

    Rebased on dev, renamed plugin and refactored to deal with new event structure

--- music_assistant/providers/lastfm/__init__.py
+++ music_assistant/providers/lastfm/__init__.py
@@ -1,7 +1,9 @@
 """Allows scrobbling of tracks with the help of PyLast."""
 
+import asyncio
 import logging
 import time
+from collections.abc import Callable
 from typing import TYPE_CHECKING
 
 import pylast
@@ -16,15 +18,16 @@ from music_assistant_models.enums import ConfigEntryType, EventType, PlayerState
 from music_assistant_models.errors import LoginFailed, SetupFailedError
 from music_assistant_models.event import MassEvent
 from music_assistant_models.media_items import Album, is_track
+from music_assistant_models.playback_progress_report import MediaItemPlaybackProgressReport
 from music_assistant_models.provider import ProviderManifest
 
 if TYPE_CHECKING:
     from music_assistant_models.media_items import Track
     from music_assistant_models.player_queue import PlayerQueue
 
-from music_assistant import MusicAssistant
 from music_assistant.constants import MASS_LOGGER_NAME
 from music_assistant.helpers.auth import AuthenticationHelper
+from music_assistant.mass import MusicAssistant
 from music_assistant.models import ProviderInstanceType
 from music_assistant.models.plugin import PluginProvider
 
@@ -33,7 +36,7 @@ async def setup(
     mass: MusicAssistant, manifest: ProviderManifest, config: ProviderConfig
 ) -> ProviderInstanceType:
     """Initialize provider(instance) with given configuration."""
-    provider = ScrobblerProvider(mass, manifest, config)
+    provider = LastFMScrobbleProvider(mass, manifest, config)
     pylast.logger.setLevel(provider.logger.level)
     if provider.logger.level == logging.DEBUG:
         # httpcore is quite spammy without providing useful information 99% of the time
@@ -42,12 +45,13 @@ async def setup(
     return provider
 
 
-class ScrobblerProvider(PluginProvider):
+class LastFMScrobbleProvider(PluginProvider):
     """Plugin provider to support scrobbling of tracks."""
 
     _network: pylast._Network = None
-    _currently_playing: str = None
+    _currently_playing: str | None = None
     _processing_queue_update = False
+    _on_unload: list[Callable[[], None]] = []
 
     def _get_network_config(self) -> dict[str, ConfigValueType]:
         return {
@@ -69,9 +73,22 @@ class ScrobblerProvider(PluginProvider):
         self._network = _get_network(self._get_network_config())
 
         # subscribe to internal events
-        self.mass.subscribe(self._on_mass_queue_updated, EventType.QUEUE_UPDATED)
-        self.mass.subscribe(self._on_mass_media_item_played, EventType.MEDIA_ITEM_PLAYED)
+        self._on_unload.append(
+            self.mass.subscribe(self._on_mass_queue_updated, EventType.QUEUE_UPDATED)
+        )
+        self._on_unload.append(
+            self.mass.subscribe(self._on_mass_media_item_played, EventType.MEDIA_ITEM_PLAYED)
+        )
         self.logger.debug("subscribed to events")
+
+    async def unload(self, is_removed: bool = False) -> None:
+        """
+        Handle unload/close of the provider.
+
+        Called when provider is deregistered (e.g. MA exiting or config reloading).
+        """
+        for unload_cb in self._on_unload:
+            unload_cb()
 
     async def _on_mass_queue_updated(self, event: MassEvent) -> None:
         """Player has updated, update nowPlaying."""
@@ -79,43 +96,52 @@ class ScrobblerProvider(PluginProvider):
             self.logger.error("no network available during _on_mass_queue_updated")
             return
 
-        if self._processing_queue_update:
-            return
+        lock = asyncio.Lock()
+        async with lock:
+            if self._processing_queue_update:
+                return
 
-        queue: PlayerQueue = event.data
-        if queue.state != PlayerState.PLAYING or not is_track(queue.current_item.media_item):
-            self.logger.debug("queue update ignored, no track currently playing")
-            self._currently_playing = None
-            return
+            queue: PlayerQueue = event.data
+            if queue.state != PlayerState.PLAYING:
+                self.logger.debug("queue update ignored, no track currently playing")
+                self._currently_playing = None
+                return
 
-        track: Track = queue.current_item.media_item
-        if track.uri == self._currently_playing:
-            self.logger.debug(
-                f"queue update ignored, track {track.uri} already marked as 'now playing'"
-            )
-            return
+            if queue.current_item is None or queue.current_item.media_item is None:
+                return
 
-        self._processing_queue_update = True
+            if not is_track(queue.current_item.media_item):
+                return
 
-        def update_now_playing() -> None:
-            try:
-                self._network.update_now_playing(
-                    track.artist_str,
-                    track.name,
-                    track.album.name if track.album else None,
-                    track.album.artist_str if isinstance(track.album, Album) else None,
-                    track.duration,
-                    track.track_number,
-                    track.mbid,
+            track: Track = queue.current_item.media_item
+            self.logger.debug(f"{track.uri} vs {self._currently_playing}")
+            if track.uri == self._currently_playing:
+                self.logger.debug(
+                    f"queue update ignored, track {track.uri} already marked as 'now playing'"
                 )
-                self.logger.debug(f"track {track.uri} marked as 'now playing'")
-                self._currently_playing = track.uri
-            except Exception as err:
-                self.logger.exception(err)
-            finally:
-                self._processing_queue_update = False
+                return
 
-        self.mass.loop.run_in_executor(None, update_now_playing)
+            self._processing_queue_update = True
+
+            def update_now_playing() -> None:
+                try:
+                    self._network.update_now_playing(
+                        track.artist_str,
+                        track.name,
+                        track.album.name if track.album else None,
+                        track.album.artist_str if isinstance(track.album, Album) else None,
+                        track.duration,
+                        track.track_number,
+                        track.mbid,
+                    )
+                    self.logger.debug(f"track {track.uri} marked as 'now playing'")
+                    self._currently_playing = track.uri
+                except Exception as err:
+                    self.logger.exception(err)
+                finally:
+                    self._processing_queue_update = False
+
+            self.mass.loop.run_in_executor(None, update_now_playing)
 
     async def _on_mass_media_item_played(self, event: MassEvent) -> None:
         """Media item has finished playing, we'll scrobble the track."""
@@ -123,34 +149,38 @@ class ScrobblerProvider(PluginProvider):
             self.logger.error("no network available during _on_mass_media_item_played")
             return
 
-        item = await self.mass.music.get_item_by_uri(event.data["media_item"])
-        if not is_track(item):
-            self.logger.debug(f"track {event.data['media_item']} skipped, as it is not a track")
-            return
+        report = event.data
 
-        track: Track = item
-
-        if event.data["seconds_played"] / track.duration < 0.5:
-            self.logger.debug(f"track {track.uri} ignored, playtime was not long enough")
+        if not self.should_scrobble(report):
+            self.logger.debug(f"track {report.uri} should not be scrobbled")
             return
 
         def scrobble() -> None:
             try:
+                # album artist and track number are not available without an extra API call
+                # so they won't be scrobbled
                 self._network.scrobble(
-                    track.artist_str,
-                    track.name,
+                    report["artist"],
+                    report["name"],
                     time.time(),
-                    track.album.name if track.album else None,
-                    track.album.artist_str if track.album else None,
-                    track.track_number,
-                    track.duration,
-                    mbid=track.mbid,
+                    report["album"],
+                    duration=report["duration"],
+                    mbid=report["mbid"],
                 )
             except Exception as err:
                 self.logger.exception(err)
 
         self.logger.debug("running scrobble() in executor")
         self.mass.loop.run_in_executor(None, scrobble)
+
+    def should_scrobble(self, report: MediaItemPlaybackProgressReport) -> bool:
+        """Determine if a track should be scrobbled, to be extended later."""
+        # ideally we want more precise control
+        # but because the event is triggered every 30s
+        # and we don't have full queue details to determine
+        # the exact context in which the event was fired
+        # we can only rely on fully_played for now
+        return bool(report.fully_played)
 
 
 # configuration keys
@@ -164,8 +194,8 @@ CONF_PROVIDER = "_provider"
 CONF_ACTION_AUTH = "_auth"
 
 # available networks
-CONF_OPTION_LASTFM = "lastfm"
-CONF_OPTION_LIBREFM = "librefm"
+CONF_OPTION_LASTFM: str = "lastfm"
+CONF_OPTION_LIBREFM: str = "librefm"
 
 
 async def get_config_entries(
@@ -181,11 +211,11 @@ async def get_config_entries(
     action: [optional] action key called from config entries UI.
     values: the (intermediate) raw values for config entries sent with the action.
     """
-    logger = logging.getLogger(MASS_LOGGER_NAME).getChild("audioscrobbler")
+    logger = logging.getLogger(MASS_LOGGER_NAME).getChild("lastfm")
 
-    provider: str = values.get(CONF_PROVIDER)
-    if values is None or not values.get(CONF_PROVIDER):
-        provider = CONF_OPTION_LASTFM
+    provider: str = CONF_OPTION_LASTFM
+    if values is not None and values.get(CONF_PROVIDER) is not None:
+        provider = str(values.get(CONF_PROVIDER))
 
     # collect all config entries to show
     entries: list[ConfigEntry] = [
@@ -195,10 +225,10 @@ async def get_config_entries(
             label="Provider",
             required=True,
             description="The endpoint to use, defaults to Last.fm",
-            options=(
+            options=[
                 ConfigValueOption(title="Last.FM", value=CONF_OPTION_LASTFM),
                 ConfigValueOption(title="LibreFM", value=CONF_OPTION_LIBREFM),
-            ),
+            ],
             default_value=provider,
             value=provider,
         ),
@@ -218,8 +248,14 @@ async def get_config_entries(
         ),
     ]
 
-    if action == CONF_ACTION_AUTH:
-        async with AuthenticationHelper(mass, str(values["session_id"])) as auth_helper:
+    # early return so we can assume values are present
+    if values is None:
+        return tuple(entries)
+
+    if action == CONF_ACTION_AUTH and values.get("session_id") is not None:
+        session_id = str(values.get("session_id"))
+
+        async with AuthenticationHelper(mass, session_id) as auth_helper:
             network = _get_network(values)
             skg = pylast.SessionKeyGenerator(network)
 
@@ -234,11 +270,11 @@ async def get_config_entries(
 
             logger.info("authenticating on %s", url)
             response = await auth_helper.authenticate(url)
-            if not response["token"]:
+            if response.get("token") is None:
                 raise LoginFailed(f"no token available in {provider} response")
 
             session_key, username = skg.get_web_auth_session_key_username(
-                url, str(response["token"])
+                url, str(response.get("token"))
             )
             values[CONF_USERNAME] = username
             values[CONF_SESSION_KEY] = session_key
@@ -249,10 +285,8 @@ async def get_config_entries(
                     type=ConfigEntryType.ALERT,
                     required=False,
                     default_value=None,
-                    label=(
-                        f"Successfully logged in as {username},",
-                        "don't forget to hit save to complete the setup",
-                    ),
+                    label=f"Successfully logged in as {username}, "
+                    "don't forget to hit save to complete the setup",
                 ),
             ]
 
@@ -304,7 +338,9 @@ def _get_network(config: dict[str, ConfigValueType]) -> pylast._Network:
     if not key or not secret:
         raise SetupFailedError("API Key and Secret need to be set")
 
-    match config.get(CONF_PROVIDER).lower():
+    provider: str = str(config.get(CONF_PROVIDER))
+
+    match provider.lower():
         case "lastfm":
             return pylast.LastFMNetwork(
                 key, secret, username=config.get(CONF_USERNAME), session_key=session_key
